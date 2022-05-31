3e49f6e9e54e7f5b2136e231f490d787eb644e6b
Leandro Fazio <leandrobfazio@gmail.com>
2022-05-31 19:32:02 -0300

    tp JS codoacodo

---
+++ javascript/script.js
@@ -0,0 +1,24 @@
+// Defino función para calculo de total entradas
+
+const valorEntrada = 200;
+
+function TotalCalc() {
+    var cantidad = window.document.getElementById('cantidad').value;
+    var categoria = window.document.getElementById('categoria').value;
+    switch (categoria) {
+        case "estudiante":
+            var descuento = 0.8;
+            break;
+        case "trainee":
+            var descuento = 0.5;
+            break;
+        case "junior":
+            var descuento = 0.15;
+            break;
+        default:
+            var descuento = 0;
+    }
+    var totalSinDescuento = cantidad * valorEntrada;
+    var descuentoTotal = totalSinDescuento * descuento;
+    document.getElementById('total').innerHTML = totalSinDescuento - descuentoTotal;
+};
